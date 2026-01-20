03dbc7795e65a6dc49b5a3e2588bca437c70a555
Zurglub <nolann.savigny@gmail.com>
2026-01-20 23:04:54 +0100

    initialize project with Express, PostgreSQL, and TypeScript setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import userRouter from "./modules/user/user.routes";
+
+export function createApp() {
+
+    const app = express();
+
+    app.use(express.json());
+
+    app.use("/users", userRouter);
+
+    app.use((req, res) => {
+        res.status(404).json({ message: 'Route not found' });
+    });
+
+    return app;
+}
