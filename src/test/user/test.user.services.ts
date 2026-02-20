b2dd4b394b1c5a9f2da361cb212f60872642b052
Zurglub <nolann.savigny@gmail.com>
2026-02-20 14:39:46 +0100

    add unit tests for UserService functionality

---
+++ src/test/user/test.user.services.ts
@@ -0,0 +1,16 @@
+import {UserService} from "../../modules/user/user.services";
+import {describe, it} from "node:test";
+
+describe('UserService', () => {
+    it('should create a user', async () => {
+        console.log('Test for creating a user');
+    })
+
+    it('should login a user', async () => {
+        console.log('Test for login a user');
+    })
+
+    it('should modify a user', async () => {
+        console.log('Test for modifying a user');
+    })
+})
